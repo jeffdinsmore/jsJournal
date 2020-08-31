040d61a19bfb3def381af7ff27d7e6fb4f345e4e
Mike Manchee <mikemanchee@gmail.com>
2020-08-31 14:16:57 -0700

    add html and basic js to project
    Co-authored-by: Jeff Dinsmore <hello@jeffdinsmore.com>

--- src/main.js
+++ src/main.js
@@ -4,5 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 $(document).ready(function() {
+  $("#journal-entry").submit(function(event) {
+    let title = $("input#journal-title").val();
+    let body = $("input#journal-body").val();
 
+    $("#result").html();
+  });
 });
